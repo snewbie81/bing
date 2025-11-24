a73613f51c969ec6d5b909650314b3a57fa0558f
snewbie81 <snewbie81@gmail.com>
2025-11-24 14:18:17 +0700

    fix for github

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/bing/', // Add this line - replace 'bing' with your repo name
 })
